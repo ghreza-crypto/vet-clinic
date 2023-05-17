a2188014601403e52e1a15e491338db7a1d2232c
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-05-18 03:37:36 +0430

    Create table animals and insert data in to it

--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@
 CREATE TABLE animals (
     name varchar(100)
 );
+
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts INT NOT NULL,
+    neutered BOOLEAN NOT NULL,
+    weight_kg DECIMAL NOT NULL
+);
