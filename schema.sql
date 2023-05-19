d142a17899b189bb6417ad79b68e815e600d7ec5
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-05-19 18:46:08 +0430

    Create tables structure in schema file

--- schema.sql
+++ schema.sql
@@ -12,3 +12,27 @@ CREATE TABLE animals (
 ALTER TABLE animals ADD species VARCHAR(100);
 
 
+CREATE TABLE owners (
+    id serial NOT NULL PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    age INT NOT NULL
+);
+
+CREATE TABLE species (
+    id serial NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD COLUMN species_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id) REFERENCES species(id);
+
+ALTER TABLE animals ADD COLUMN owner_id INT;
+
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id) REFERENCES owners(id);
