f9b0e82387f7d67aa80a9830ea3a03b1802ff55f
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-05-19 10:35:16 +0430

    Add transactions to queries and add species column

--- queries.sql
+++ queries.sql
@@ -15,3 +15,35 @@ SELECT * from animals WHERE neutered = TRUE;
 SELECT * from animals WHERE name != 'Gabumon';
 
 SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+
+--1 Transaction 
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+--2 transaction
+BEGIN TRANSACTION;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
+COMMIT;
+SELECT * FROM animals; 
+
+--3 Transaction 
+BEGIN TRANSACTION;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+
+-- 4 Transaction 
+BEGIN TRANSACTION;
+DELETE from animals WHERE date_of_birth > '2022/01/01';
+SAVEPOINT animals_after_2022;
+UPDATE animals SET weight_kg = (weight_kg * -1);
+ROLLBACK TO SAVEPOINT animals_after_2022;
+UPDATE animals SET weight_kg = (weight_kg * -1) WHERE weight_kg < 0;
+COMMIT;
+SELECT * from animals;
+
+
