bbbe005d8b0981fad3d0021a2b66aaaa08879a36
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-05-19 11:02:10 +0430

    Add queries for questions

--- schema.sql
+++ schema.sql
@@ -11,3 +11,22 @@ CREATE TABLE animals (
 
 ALTER TABLE animals ADD species VARCHAR(100);
 
+--How many animals are there?
+SELECT COUNT(*) from animals;
+
+--How many animals have never tried to escape?
+SELECT COUNT(*) from animals WHERE escape_attempts = 0;
+
+--What is the average weight of animals?
+SELECT AVG(weight_kg) from animals;
+
+--Who escapes the most, neutered or not neutered animals?
+SELECT neutered, SUM(escape_attempts) from animals GROUP BY neutered;
+
+--What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg), MAX(weight_kg) from animals GROUP BY species;
+
+--What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) from animals 
+WHERE date_of_birth BETWEEN '1990/01/01' AND '2000/01/01'
+GROUP BY species;
