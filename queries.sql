5729702b28168549a8311175846aa97d4163a077
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-05-19 11:04:11 +0430

    Fix the partitions

--- queries.sql
+++ queries.sql
@@ -46,4 +46,24 @@ UPDATE animals SET weight_kg = (weight_kg * -1) WHERE weight_kg < 0;
 COMMIT;
 SELECT * from animals;
 
+--How many animals are there?
+SELECT COUNT(*) from animals;
+
+--How many animals have never tried to escape?
+SELECT COUNT(*) from animals WHERE escape_attempts = 0;
+
+--What is the average weight of animals?
+SELECT AVG(weight_kg) from animals;
+
+--Who escapes the most, neutered or not neutered animals?
+SELECT neutered, SUM(escape_attempts) from animals GROUP BY neutered;
+
+--What is the minimum and maximum weight of each type of animal?
+SELECT species, MIN(weight_kg), MAX(weight_kg) from animals GROUP BY species;
+
+--What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species, AVG(escape_attempts) from animals 
+WHERE date_of_birth BETWEEN '1990/01/01' AND '2000/01/01'
+GROUP BY species;
+
 
