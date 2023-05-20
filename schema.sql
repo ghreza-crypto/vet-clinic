5c9817f20c5a4f2af3cd77b01027c13ea94912a9
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-05-20 19:50:44 +0430

    Update schema file for adding 3 new table

--- schema.sql
+++ schema.sql
@@ -36,3 +36,23 @@ ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals
 ADD CONSTRAINT fk_owner
 FOREIGN KEY (owner_id) REFERENCES owners(id);
+
+CREATE TABLE vets (
+    id SERIAL NOT NULL PRIMARY KEY,
+	name VARCHAR(50) NOT NULL,
+	age INT NOT NULL,
+	date_of_graduation DATE NOT NULL
+);
+
+CREATE TABLE specializations (
+    id SERIAL NOT NULL PRIMARY KEY,
+	vet_id INT REFERENCES vets(id),
+	specie_id INT REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+    id SERIAL NOT NULL PRIMARY KEY,
+	vet_id INT REFERENCES vets(id),
+	animal_id INT REFERENCES animals(id),
+	date_of_visit DATE NOT NULL
+);
