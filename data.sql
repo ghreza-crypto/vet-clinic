dd8a018eb46df394bd95fbdd64e2ff7ec4d23045
ghulam reza rajabi <ghulamerezarajabe@gmail.com>
2023-05-20 19:54:07 +0430

    Add data to data file

--- data.sql
+++ data.sql
@@ -42,3 +42,40 @@ UPDATE animals SET owner_id = 2 WHERE name IN ('Gabumon', 'Pikachu');
 UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
 UPDATE animals SET owner_id = 4 WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
 UPDATE animals SET owner_id = 5 WHERE name IN ('Angemon', 'Boarmon');
+
+INSERT INTO vets(name, age, date_of_graduation)
+VALUES 
+('William Tatcher', 45, '2000/04/23'),
+('Maisy Smith', 26, '2019/01/17'),
+('Stephanie Mendez', 64, '1981/05/04'),
+('Jack Harkness', 38, '2008/06/08');
+
+INSERT INTO specializations(vet_id, specie_id)
+VALUES 
+(1, 1),
+(3, 1),
+(3, 2),
+(4, 2);
+
+INSERT INTO visits(vet_id, animal_id, date_of_visit)
+VALUES 
+(1, 1, '2020/05/24'),
+(3, 1, '2020/07/22'),
+(4, 2, '2021/02/02'),
+(2, 3, '2020/01/05'),
+(2, 3, '2020/03/08'),
+(2, 3, '2020/05/14'),
+(3, 4, '2021/05/04'),
+(4, 5, '2021/02/24'),
+(2, 6, '2019/12/21'),
+(1, 6, '2020/08/10'),
+(2, 6, '2021/04/07'),
+(3, 9, '2019/09/29'),
+(4, 8, '2020/10/03'),
+(4, 8, '2020/11/04'),
+(2, 7, '2019/01/24'),
+(2, 7, '2019/05/15'),
+(2, 7, '2020/02/27'),
+(3, 7, '2020/05/24'),
+(1, 10, '2021/01/11');
+
